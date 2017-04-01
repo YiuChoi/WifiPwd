33f97a05f30266bcc0d45c59ed0987892d6621b1
蔡小木 <whucaiyao@gmail.com>
2017-04-01 17:31:23 +0800

    123

---
+++ app/src/main/java/name/caiyao/wifipwd/MainContract.java
@@ -0,0 +1,12 @@
+package name.caiyao.wifipwd;
+
+import name.caiyao.wifipwd.base.BaseView;
+
+/**
+ * Created by xiaomu on 2017/4/1.
+ */
+
+public interface MainContract {
+
+  interface View extends BaseView<Presenter>
+}
---
+++ app/src/main/java/name/caiyao/wifipwd/base/BasePresenter.java
@@ -0,0 +1,9 @@
+package name.caiyao.wifipwd.base;
+
+/**
+ * Created by xiaomu on 2017/4/1.
+ */
+
+public interface BasePresenter {
+  void start();
+}
